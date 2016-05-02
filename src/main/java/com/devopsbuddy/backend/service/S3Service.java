d429fcaa08ff77e8c904f108ee1ec2d86c801ebf
Marco Tedone <marco.tedone@gmail.com>
2016-05-02 21:34:51 +0100

    Completed the S3 Service

---
+++ src/main/java/com/devopsbuddy/backend/service/S3Service.java
@@ -0,0 +1,167 @@
+package com.devopsbuddy.backend.service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.GroupGrantee;
+import com.amazonaws.services.s3.model.Permission;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by tedonema on 02/05/2016.
+ */
+@Service
+public class S3Service {
+
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(S3Service.class);
+
+    @Value("${aws.s3.root.bucket.name}")
+    private String bucketName;
+
+    @Value("${aws.s3.profile}")
+    private String awsProfileName;
+
+    @Value("${image.store.tmp.folder}")
+    private String tempImageStore;
+
+    @Autowired
+    private AmazonS3Client s3Client;
+
+    /**
+     * Returns the root URL where the bucket name is located.
+     * <p>Please note that the URL does not contain the bucket name</p>
+     * @param bucketName The bucket name
+     * @return the root URL where the bucket name is located.
+     */
+    public String ensureBucketExists(String bucketName) {
+
+        String bucketUrl = null;
+
+        try {
+            if (!s3Client.doesBucketExist(bucketName)) {
+                LOG.info("Bucket {} doesn't exists...Creating one");
+                s3Client.createBucket(bucketName);
+                LOG.info("Created bucket: {}", bucketName);
+            }
+            bucketUrl = s3Client.getResourceUrl(bucketName, null) + bucketName;
+        } catch (AmazonClientException ace) {
+            LOG.error("An error occurred while connecting to S3. Will not execute action" +
+                    " for bucket: {}", bucketName, ace);
+        }
+
+
+        return bucketUrl;
+    }
+
+    /**
+     * It stores the given file name in S3 and returns the key under which the file has been stored
+     * @param resource The file resource to upload to S3
+     * @return The URL of the uploaded resource or null if a problem occurred
+     *
+     * @throws IllegalArgumentException If the resource file does not exist
+     */
+    public String storeProfileImage(File resource, String username) {
+
+        String resourceUrl = null;
+
+        if (!resource.exists()) {
+            LOG.error("The file {} does not exist. Throwing an exception", resource.getAbsolutePath());
+            throw new IllegalArgumentException("The file " + resource.getAbsolutePath() + " doesn't exist");
+        }
+
+        String rootBucketUrl = this.ensureBucketExists(bucketName);
+
+        if (null == rootBucketUrl) {
+
+            LOG.error("The bucket {} does not exist and the application " +
+                    "was not able to create it. The image won't be stored with the profile", rootBucketUrl);
+
+        } else {
+
+            AccessControlList acl = new AccessControlList();
+            acl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+
+            String key = username + "/profilePicture." + FilenameUtils.getExtension(resource.getName());
+
+            try {
+                s3Client.putObject(new PutObjectRequest(bucketName, key, resource).withAccessControlList(acl));
+                resourceUrl = s3Client.getResourceUrl(bucketName, key);
+            } catch (AmazonClientException ace) {
+                LOG.error("A client exception occurred while trying to store the profile" +
+                        " image {} on S3. The profile image won't be stored", resource.getAbsolutePath(), ace);
+            }
+        }
+
+        return resourceUrl;
+
+    }
+
+    /**
+     * It stores the given file name in S3 and returns the key under which the file has been stored
+     * @param uploadedFile The multipart file uploaed by the user
+     * @param username The username for which to upload this file
+     * @return The URL of the uploaded image
+     */
+    public String storeProfileImage(MultipartFile uploadedFile, String username) throws
+            IOException {
+
+        String profileImageUrl = null;
+
+        if (uploadedFile != null && !uploadedFile.isEmpty()) {
+            byte[] bytes = uploadedFile.getBytes();
+
+            // The root of our temporary assets. Will create if it doesn't exist
+            File tmpImageStoredFolder = new File(tempImageStore + File.separatorChar + username);
+            if (!tmpImageStoredFolder.exists()) {
+                LOG.info("Creating the temporary root for the S3 assets");
+                tmpImageStoredFolder.mkdirs();
+            }
+
+            // The temporary file where the profile image will be stored
+            File tmpProfileImageFile = new File(tmpImageStoredFolder.getAbsolutePath()
+                    + File.separatorChar
+                    + "profileImage."
+                    + FilenameUtils.getExtension(uploadedFile.getOriginalFilename()));
+
+            LOG.info("Temporary file will be saved to {}", tmpProfileImageFile.getAbsolutePath());
+
+            try(BufferedOutputStream stream =
+                        new BufferedOutputStream(
+                                new FileOutputStream(new File(tmpProfileImageFile.getAbsolutePath())))) {
+                stream.write(bytes);
+            }
+
+            profileImageUrl = storeProfileImage(tmpProfileImageFile, username);
+
+            // Clean up the temporary folder
+            tmpProfileImageFile.delete();
+        }
+
+        return profileImageUrl;
+
+    }
+
+    /**
+     * Returns the URL of the resource in the given bucket
+     * @param bucketName The bucket containing the resource
+     * @param fileKey The key identifying the resource
+     * @return the URL of the resource in the given bucket
+     */
+    public String retrieveResource(String bucketName, String fileKey) {
+        return s3Client.getResourceUrl(bucketName, fileKey);
+
+    }
+}
