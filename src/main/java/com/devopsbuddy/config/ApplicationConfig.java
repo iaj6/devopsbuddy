3d6e45c68715f84650fd825c0a027fd5677a1748
Marco Tedone <marco.tedone@gmail.com>
2016-06-29 21:50:28 +0100

    Modified application to run in PCF

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package com.devopsbuddy.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,8 +18,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "com.devopsbuddy.backend.persistence.repositories")
 @EntityScan(basePackages = "com.devopsbuddy.backend.persistence.domain.backend")
 @EnableTransactionManagement
-@PropertySource("file:///${user.home}/.devopsbuddy/application-common.properties")
-@PropertySource("file:///${user.home}/.devopsbuddy/stripe.properties")
 public class ApplicationConfig {
 
     @Value("${aws.s3.profile}")
@@ -29,8 +25,9 @@ public class ApplicationConfig {
 
     @Bean
     public AmazonS3Client s3Client() {
-        AWSCredentials credentials = new ProfileCredentialsProvider(awsProfileName).getCredentials();
-        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+
+        //AWS credentials are in the {app}-common.properties file
+        AmazonS3Client s3Client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
         Region region = Region.getRegion(Regions.EU_WEST_1);
         s3Client.setRegion(region);
         return s3Client;
--- src/main/java/com/devopsbuddy/config/ProductionConfig.java
+++ src/main/java/com/devopsbuddy/config/ProductionConfig.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by tedonema on 21/03/2016.
  */
 @Configuration
 @Profile("prod")
-@PropertySource("file:///${user.home}/.devopsbuddy/application-prod.properties")
 public class ProductionConfig {
 
     @Value("${stripe.prod.private.key}")
